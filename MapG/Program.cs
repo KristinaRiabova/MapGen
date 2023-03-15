418f2d919a7a4e32ce0968e6d8ca32f2cac03691
Kristina <kristinaer304@gmail.com>
2023-03-15 15:27:13 +0200

    Add speed and time

--- MapG/Program.cs
+++ MapG/Program.cs
@@ -13,7 +13,7 @@ bool IsWall(string c)
 {
     return c == "█";
 }
-
+int [] counter = new int[9];
 void PrintMapWithPath(string[,] map, List<Point> path)
 {
     Point start = path[0];
@@ -31,10 +31,44 @@ void PrintMapWithPath(string[,] map, List<Point> path)
         }
         else
         {
+            switch (map[p.Column, p.Row])
+            {
+                case "1":
+                    counter[0]++;
+                    break;
+                case "2":
+                    counter[1]++;
+                    break;
+                case "3":
+                    counter[2]++;
+                    break;
+                case "4":
+                    counter[3]++;
+                    break;
+                case "5":
+                    counter[4]++;
+                    break;
+                case "6":
+                    counter[5]++;
+                    break;
+                case "7":
+                    counter[6]++;
+                    break;
+                case "8":
+                    counter[7]++;
+                    break;
+                case "9":
+                    counter[8]++;
+                    break;
+            }
             map[p.Column, p.Row] = ".";
         }
     }
 
+    for (int i = 0; i < counter.Length; i++)
+    {
+        Console.WriteLine("Symbol " + (i+1) + " in maze : " + counter[i] + " times");
+    }
     new MapPrinter().Print(map);
 }
 
@@ -72,7 +106,6 @@ List<Point> SearchDijkstra(string[,] map, Point start, Point end)
     
     Dictionary<Point, Point?> previous = new Dictionary<Point, Point?>();
 
-    
     
     foreach (int i in Enumerable.Range(0, map.GetLength(0)))
     {
@@ -117,9 +150,10 @@ List<Point> SearchDijkstra(string[,] map, Point start, Point end)
                 previous[neighbour] = currentPoint;
             }
         }
+
     }
     
-    
+   
     List<Point> path = new List<Point>();
     Point? current = end;
     
@@ -133,12 +167,13 @@ List<Point> SearchDijkstra(string[,] map, Point start, Point end)
     {
         
         path.Add(current.Value);
-        
+       
         current = previous[current.Value];
     }
-     
+    
     
     path.Reverse();
+    Console.WriteLine(distance[end]-1);
     return path;
 }
 /*List<Point> SearchBFS(string[,] map, Point start, Point end)
@@ -183,14 +218,37 @@ List<Point> SearchDijkstra(string[,] map, Point start, Point end)
         Height = height,
         Width = width,
         Seed = 3234,
-        Noise = 0.1f
+        Noise = 0.1f,
+        AddTraffic = true,
+        TrafficSeed = 34322264
     });
 
     string[,] map = generator.Generate();
 
 
+
+double time = 0;
+
+int[] neededKms = new int[9];
+
+for (int i = 0; i < neededKms.Length; i++)
+{
+    neededKms[i] = 60 - ((i + 1) - 1) * 6;
+}
+
+
+
+    
     Point start = new Point(0, 0);
     Point end = new Point(29, 7);
     List<Point> path = SearchDijkstra(map, start, end);
-    Console.WriteLine("");
     PrintMapWithPath(map, path);
+    for (int i = 0; i < counter.Length; i++)
+    {
+        if (counter[i] == 0) continue;
+        else
+        {
+            time += 60 * counter[i] / neededKms[i];
+        }
+    }
+Console.WriteLine(time + " mins");
